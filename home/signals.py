dd0a286933c2c0c3d30b1ce61e3a8ac8c3fbb2ad
Mohd Numan <48924562+one-numan@users.noreply.github.com>
2025-03-16 00:04:51 +0530

    Update Signals

--- home/signals.py
+++ home/signals.py
@@ -9,15 +9,19 @@ from loguru import logger
 
 logger.add("signal.log")
 
+
+####  In Built Signals  #
 # Pre_save Dispatch
 @receiver(pre_save,sender=Car)
 def signal_new_car_add(sender,instance,**kwargs):
+    # This Is Execute Before adding a New Car
     print("Signals : New Car Added")
     print(F"Signals : Sender : {sender} \n Instance : {instance}")
 
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, created, **kwargs):
+    # This is Execute After adding a New Car and Also showing the all cars
     if created:
         logger.info(f"New car added: {instance.car_name}")
         # logger.warning("Here is an Error")
@@ -30,7 +34,19 @@ def car_post_save(sender, instance, created, **kwargs):
             print(f"- {car.car_name}")
 
 
+@receiver(pre_delete,sender=Car)
+def car_pre_delete(sender,instance,**kwargs):
+    print(F"Sender : {sender}")
+    print(F"Car : {instance.car_name} is try to delete")
+
+
 @receiver(post_delete,sender=Car)
 def car_post_delete(sender,instance,**kwargs):
+    print(kwargs)
     print(F"Car Deleted : {instance.car_name}")
+    # Using PermissionDenied After Deletion Of Car, Its execute and RoleBack Applied 
+    # In_Short : Car is Deleted but after Error Occurs its roleback and Deleted cars is back into the DB
     raise PermissionDenied("Cars cannot be deleted via signal.")
+
+
+
